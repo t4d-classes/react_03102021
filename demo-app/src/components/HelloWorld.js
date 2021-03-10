ad81089314a2db5ab5fc1c8c0806b81cee60cdff
Eric Greene <eric@t4d.io>
2021-03-10 14:04:00 -0500

    add exercise 1

---
+++ demo-app/src/components/HelloWorld.js
@@ -0,0 +1,8 @@
+export function HelloWorld() {
+  return (
+    <>
+      <h1>Hello, World!</h1>
+      <span>test</span>
+    </>
+  );
+}
---
+++ demo-app/src/index.js
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+
+import { HelloWorld } from "./components/HelloWorld";
+
+render(<HelloWorld />, document.querySelector("#root"));
