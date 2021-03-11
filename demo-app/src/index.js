a7940cbe6f955ea93afd17cc3c80b1d2bfa64325
Eric Greene <eric@t4d.io>
2021-03-11 06:40:25 -0500

    add exercise 4 solution

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -1,22 +1,39 @@
-import { render } from "react-dom";
+import { render } from 'react-dom';
 
-import { ColorTool } from "./components/ColorTool";
-
-// model data
+import { ColorTool } from './components/ColorTool';
+import { CarTool } from './components/CarTool';
 
 const colorList = [
-  { id: 1, name: "red", hexcode: "ff0000" },
-  { id: 2, name: "blue", hexcode: "0000ff" },
-  { id: 3, name: "green", hexcode: "00ff00" },
-  { id: 4, name: "orange", hexcode: "ffff00" },
-  { id: 5, name: "yellow", hexcode: "00ffff" },
+  { id: 1, name: 'red', hexcode: 'ff0000' },
+  { id: 2, name: 'blue', hexcode: '0000ff' },
+  { id: 3, name: 'green', hexcode: '00ff00' },
+  { id: 4, name: 'orange', hexcode: 'ffff00' },
+  { id: 5, name: 'yellow', hexcode: '00ffff' },
 ];
-//                            props
-// React.createElement(Color, { colors: colorList } )
+
+const carList = [
+  {
+    id: 1,
+    make: 'Ford',
+    model: 'Fusion Hybrid',
+    year: 2019,
+    color: 'red',
+    price: 45000,
+  },
+  {
+    id: 2,
+    make: 'Tesla',
+    model: 'S',
+    year: 2020,
+    color: 'blue',
+    price: 120000,
+  },
+];
 
 render(
   <>
     <ColorTool colors={colorList} />
+    <CarTool cars={carList} />
   </>,
-  document.querySelector("#root")
+  document.querySelector('#root'),
 );
